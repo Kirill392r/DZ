a52deba3dc7399da558042a1454ae22482d0b43e
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-02-04 17:49:33 +0300

    Добавлена функция get_date и проведены итоговые проверки программы

---
+++ src/widget.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(info: str) -> str:
+    """Маскирует номер карты или счёта в зависимости от типа."""
+    if info.startswith("Счет"):
+        masked_account = info.replace("Счет", "").strip()
+        masked_number = get_mask_account(masked_account)
+        return f"Счет {masked_number}"
+    else:
+        parts = info.split()
+        masked_card_number = parts[-1]
+        masked_number = get_mask_card_number(masked_card_number)
+        return " ".join(parts[:-1]) + " " + masked_number
+
+
+def get_date(date_str: str) -> str:
+    """Преобразует строку с датой в формате "2024-03-11T02:26:18.671407"
+    в строку с датой в формате "ДД.ММ.ГГГГ"."""
+    try:
+        date_obj = datetime.fromisoformat(date_str)
+        formatted_date = date_obj.strftime("%d.%m.%Y")
+        return formatted_date
+    except ValueError:
+        return "Некорректный формат даты"
