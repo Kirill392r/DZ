1f04a12c934642939672e3cdd218bd5838817067
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-02-11 17:15:35 +0300

    добавлена функция 'filter_by_state', произведена проверка на black, flake8, mypy

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def filter_by_state(list_dikt: list[dict], state: str) -> list[dict]:
+    """Функция которая возвращает новый список словарей, содержащий только те словари,
+    у которых ключ соответствует указанному значению."""
+    new_list_dikt = []
+    for dict in list_dikt:
+        if dict.get("state") == state:
+            new_list_dikt.append(dict)
+    return new_list_dikt
