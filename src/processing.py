b184b79acd57a23b7031564c8a310af4a7675d9e
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-02-12 13:03:01 +0300

    добавление новой функции sort_by_date, проверка кода на литеры

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,9 @@ def filter_by_state(list_dikt: list[dict], state: str = 'EXECUTED') -> list[dict
         if dict.get("state") == state:
             new_list_dikt.append(dict)
     return new_list_dikt
+
+
+def sort_by_date(data: list[dict], reverse: bool = True) -> list:
+    """Функция которая возвращает новый список, отсортированный по дате"""
+
+    return sorted(data, key=lambda x: x["date"], reverse=reverse)
