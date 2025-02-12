e9da1a7f8670f16f0885167016f6f4eeb13088df
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-02-12 15:37:07 +0300

    изменено имя переменной с 'dict' на 'i'

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,10 @@
-def filter_by_state(list_dikt: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(list_dikt: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция которая возвращает новый список словарей, содержащий только те словари,
     у которых ключ соответствует указанному значению."""
     new_list_dikt = []
-    for dict in list_dikt:
-        if dict.get("state") == state:
-            new_list_dikt.append(dict)
+    for i in list_dikt:
+        if i.get("state") == state:
+            new_list_dikt.append(i)
     return new_list_dikt
 
 
