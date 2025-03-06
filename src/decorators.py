8f789336e8e50d6c92521445b38c6d4815df5b0d
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-03-06 16:43:16 +0300

    fixes decorator

--- src/decorators.py
+++ src/decorators.py
@@ -1,17 +1,12 @@
 def log(filename=None):
     """Функция декоратор , который будет автоматически логировать начало и конец выполнения функции"""
 
-    def wrapper(func):
+    def inner(func):
         """Функция обёртка принимает функцию которую необходимо декорировать"""
 
-        def inner(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             """Внутринняя функция"""
             try:
-                if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{func.__name__}, Inputs: {', '.join(map(str, args))}, {kwargs}\n")
-                else:
-                    print(f"Запуск функции {func.__name__}, Inputs: {', '.join(map(str, args))}, {kwargs}\n")
                 result = func(*args, **kwargs)
                 if filename:
                     with open(filename, "a") as file:
@@ -21,12 +16,13 @@ def log(filename=None):
             except Exception as e:
                 error_massage = f"{e} error: тип ошибки. Inputs: {args}, {kwargs}\n"
                 if filename:
-                    with open(filename, "a") as file:
+                    with open(filename, "a", encoding="utf-8") as file:
                         file.write(f"{error_massage}\n")
                 else:
                     print(error_massage)
-            return result
+                    return e
+                return result
 
-        return inner
+        return wrapper
 
-    return wrapper
+    return inner
