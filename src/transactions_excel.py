f525fff432ab71c743e4bf49ea3e583e518c3e9b
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-03-20 14:56:59 +0300

    correction of the description

--- src/transactions_excel.py
+++ src/transactions_excel.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read_transaction_exel(file) -> list[dict]:
-    """Функция которая считывает данные с Excel файла"""
+    """Функция которая считывает данные с Excel-файла"""
     try:
         if not file:
             return []
