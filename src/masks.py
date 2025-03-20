3725e3e91dc5e875c65d85b5b68a35c0f0e0c2e3
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-03-20 14:42:26 +0300

    correction log path

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 import logging
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler("..\\logs\\masks.log", "w", encoding="utf8")
+file_handler = logging.FileHandler("logs/masks.log", "w", encoding="utf8")
 file_formater = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formater)
 logger.addHandler(file_handler)
