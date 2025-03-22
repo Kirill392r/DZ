6221b54597c9630de875e3598710d366c9f50aa8
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-03-22 20:02:55 +0300

    add func

---
+++ src/data_search.py
@@ -0,0 +1,30 @@
+import re
+
+
+def regular_expression_data_search(data: list[dict], key_transaction: str) -> list[dict]:
+    try:
+        if not data:
+            return []
+        pattern = re.compile(r":", key_transaction)
+        findal = pattern.search(data)
+        return findal
+    except Exception:
+        return []
+
+
+print(regular_expression_data_search([
+  {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }], "state"))
