8f60a4ab51abaccb2a5004171beb5504164c2f9b
кирилл расторгуев <kirillrastorguev5@gmail.com>
2025-03-24 17:07:02 +0300

    add Path data

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+CURRENT_DIR = Path(__file__).parent
+
+TRANSACTION_PATH_JSON = CURRENT_DIR / "data" / "operations.json"
+
+TRANSACTION_PATH_CSV = CURRENT_DIR / "data" / "transactions.csv"
+
+TRANSACTION_PATH_XLSX = CURRENT_DIR / "data" / "transactions_excel.xlsx"
